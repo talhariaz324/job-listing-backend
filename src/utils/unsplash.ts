7a3c0a47c66e85f71788535b6e50902cd7a6e9c3
talhariaz324 <talhariaz324@gmail.com>
2024-11-01 12:10:15 +0500

    Cleaning Code

--- src/utils/unsplash.ts
+++ src/utils/unsplash.ts
@@ -11,20 +11,17 @@ export const processJob = async (newJob: Job, jobs: Job[]) => {
       const jobIndex = currentJobs.findIndex((job) => job.id === newJob.id);
 
       if (jobIndex === -1) return;
-      console.log(process.env.UNSPLASH_ACCESS_KEY);
 
       const response = await axios.get(
         "https://api.unsplash.com/photos/random",
         {
-          params: { query: "food"},
+          params: { query: "food" },
           headers: {
             "Accept-Version": "v1",
             Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
           },
         }
       );
-      console.log(response.data.urls.full);
-      
       // Update the specific job in the fresh copy
       currentJobs[jobIndex].status = "resolved";
       currentJobs[jobIndex].result = response.data.urls.full;
